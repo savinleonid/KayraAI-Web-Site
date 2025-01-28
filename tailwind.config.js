51acfcdb3c4fcdce123a2fe34d91db2712f3fa7c
Leonid Savin <110335131+savinleonid@users.noreply.github.com>
2025-01-29 05:48:37 +0700

    Initial commit from your app

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      colors: {
+        kayra: {
+          primary: '#7FE0DB',
+          dark: '#1B4259',
+          darker: '#15374A',
+        },
+      },
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      }
+    },
+  },
+  plugins: [],
+};
